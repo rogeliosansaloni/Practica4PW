5241b5187edbecf297e829d3163e07cc8f4fce32
Bn32w <bernardonouel8@gmail.com>
2020-01-25 17:35:53 +0100

    Cambiar cosas

--- JavaScipt/HeaderJS.js
+++ JavaScipt/HeaderJS.js
@@ -27,7 +27,7 @@ class Gafa {
         this.bgColor = data_gafa.bgColor;
     }
 }
-/*
+
 window.onload = function() {
 
     document.getElementById("x").addEventListener('click', function () {
@@ -55,7 +55,7 @@ window.onload = function() {
       });
 
 }
-*/
+
 
   function actualizaMenu (n_gafas) {
     console.log("brilli brilli");
